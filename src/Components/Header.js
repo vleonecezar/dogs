47875337e9e3de0b72343101c2db5eb19cec0ab7
Vitor Leone Cezar <vleone.social@gmail.com>
2021-08-20 11:23:26 -0300

    assets, header e seus estilos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div style={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
